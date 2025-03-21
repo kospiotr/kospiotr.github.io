fa45531acd3e077adf2424d4168e9a5b4cb768a6
Piotr Kosmowski <kospiotr@gmail.com>
2025-03-21 08:41:06 +0100

    Update

---
+++ functions/api/apps/test.ts
@@ -0,0 +1,9 @@
+interface Env {
+    KV: KVNamespace;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+    const value = await context.env.KV.get("example");
+    const params = context.params;
+    return new Response(JSON.stringify({value, params}));
+};
