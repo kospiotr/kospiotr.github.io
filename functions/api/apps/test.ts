e21063f202719c9021ec132389463d9ac5b78cb9
Piotr Kosmowski <kospiotr@gmail.com>
2025-03-21 11:07:36 +0100

    Update

--- functions/api/apps/test.ts
+++ functions/api/apps/test.ts
@@ -5,5 +5,8 @@ interface Env {
 export const onRequest: PagesFunction<Env> = async (context) => {
     const value = await context.env;
     const params = context.params;
-    return new Response(JSON.stringify({value, params}));
+    const data = context.data;
+    const headers = context.request.headers;
+    const text = await context.request.text();
+    return new Response(JSON.stringify({value, params, data, headers, text}));
 };
